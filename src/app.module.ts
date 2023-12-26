123dc40eea461725313be51217d85d2dc4560d63
Saleh Alikhani <salehalikhani@gmail.com>
2023-12-26 04:59:38 +0330

    infuse with gql and mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { MongooseModule } from '@nestjs/mongoose';
+import { config } from 'dotenv';
+
+config();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGODB_URI as string, {
+      user: process.env.MONGODB_USER,
+      pass: process.env.MONGODB_PASS,
+    }),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useFactory: async () => ({
+        cors: true,
+        debug: true,
+        playground: true,
+        autoSchemaFile: 'src/schema.gql',
+        autoTransformHttpErrors: true,
+      }),
+    }),
+  ],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    helmet({
+      crossOriginResourcePolicy: false,
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: false,
+    }),
+  );
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
